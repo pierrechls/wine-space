7544a130549a88f8883326c80f24e53e50a88172
Pierre Charles <pierre.chls@gmail.com>
2016-12-10 12:51:05 +0100

    FIX home slider

--- wine-space/index.php
+++ wine-space/index.php
@@ -3,16 +3,14 @@
  *
  * @package winespace
  */
-
  include (TEMPLATEPATH . '/to-cart.php');
  include (TEMPLATEPATH . '/mail-wine-grower.php');
  
  get_header(); ?>
         
-         <div class="main-container" style="background-color: rgba(247,247,247,0.8)">
-            <article id="cuvees">
-                <div class="bxslider">
-                	<ul class="home-slider">
+         <div class="home-container" style="background-color: rgba(247,247,247,0.8); height:100vh;">
+            <div class="bxslider" style="height:100vh;">
+                	<ul class="home-slider" style="height:100vh;">
 					
 					<?php 
 				
@@ -20,7 +18,7 @@
 						while (have_posts()) : the_post();
 					
 					?>
-							<li><a href='<?php the_permalink() ?>'><?php the_title(); ?></a></li>
+							<li style="background-image:url('<?php the_post_thumbnail_url( 'full' ); ?>');"></li>
 					<?php 
 					
 						endwhile;
@@ -29,34 +27,27 @@
 					<?php wp_reset_query(); ?>
 					
 					</ul>
-		        </div>
-            </article>
+		    </div>
         </div>
         
 		</div> <!-- end content div -->
 		</div> <!-- end container div -->
 		
 		<script type="text/javascript"> 
-		        var j = jQuery.noConflict();
-				
-				var maxSlides, minSlides, width = j(window).width();
-				
-				if (width < 430) {
-					minSlides = 1;
-				    maxSlides = 1;
-				} else {
-					minSlides = 2;
-				    maxSlides = 2;
-				}
+			var j = jQuery.noConflict();
 		
-		        j(function ($) {
-		        j(document).ready(function(){
-		          j('.home-slider').bxSlider({
-		            mode: 'horizontal',
-		            captions: false
-		          });
-		        }); 
-		        });
+		    j(function ($) {
+				j(document).ready(function(){
+		        	j('.home-slider').bxSlider({
+		            	mode: 'horizontal',
+						captions: false,
+						pager: false,
+						maxSlides: 1,
+						minSlides: 1,
+						adaptiveHeight: true
+					});
+				}); 
+		    });
 	   </script>
 
 <?php get_footer(); ?>
