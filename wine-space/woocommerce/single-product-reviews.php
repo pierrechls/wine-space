fb9e5d7dfb2d3ea3b2ff681e57daea15d580bf4a
Pierre Charles <pierre.chls@gmail.com>
2017-01-11 13:58:48 +0100

    Remove template folder

--- wine-space/woocommerce/single-product-reviews.php
+++
@@ -1,8 +0,0 @@
-<?php
-/**
- * Display single product reviews (comments)
- *
- * @author 		WooThemes
- * @package 	WooCommerce/Templates
- * @version     2.3.2
- */
--- wine-space/woocommerce/single-product/tabs/additional-information.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * Additional Information tab
- *
- * @author        WooThemes
- * @package       WooCommerce/Templates
- * @version       2.0.0
- */
-
