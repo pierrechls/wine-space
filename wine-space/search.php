35dd82e866275a8fac8d8488eb5e0aa745016701
Pierre Charles <pierre.chls@gmail.com>
2017-05-03 22:24:27 +0200

    :sparkles: Redirect to post_type search

--- wine-space/search.php
+++ wine-space/search.php
@@ -4,35 +4,52 @@
  *
  * @package winespace
  */
- 
- wp_redirect( home_url(), 301 );
-    exit; ?>
-
-<?php 
-/*
-get_header(); ?>
-
-	<section id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
-
-		<?php if ( have_posts() ) : ?>
-
-			<header class="page-header">
-				<h1 class="page-title"><?php printf( __( 'Search Results for: %s', 'winespace' ), '<span>' . get_search_query() . '</span>' ); ?></h1>
-			</header><!-- .page-header -->
-
-			<?php get_template_part( 'loop' ); ?>
-
-		<?php else : ?>
-
-			<?php get_template_part( 'content', 'none' ); ?>
-
-		<?php endif; ?>
-
-		</main><!-- #main -->
-	</section><!-- #primary -->
-
-<?php do_action( 'winespace_sidebar' ); ?>
-<?php get_footer(); ?>
-*/
 ?>
+
+	<?php  
+	
+		$post_type = get_query_var( 'post_type', '' );
+		if($post_type != 'domaine') {
+			wp_redirect( home_url(), 301 );
+			exit;
+		}
+		
+		else {
+		
+			if ( isset( $post_type ) && locate_template( 'search-' . $post_type . '.php' ) ) {
+			  get_template_part( 'search', $post_type );
+			  exit;
+			} 
+			
+			else {
+		
+	?>
+	
+			<? get_header(); ?>
+		
+				<section id="primary" class="content-area">
+					<main id="main" class="site-main" role="main">
+			
+					<?php if ( have_posts() ) : ?>
+			
+						<header class="page-header">
+							<h1 class="page-title"><?php printf( __( 'Search Results for: %s', 'winespace' ), '<span>' . get_search_query() . '</span>' ); ?></h1>
+						</header><!-- .page-header -->
+						
+						<?php get_template_part( 'loop' ); ?>
+			
+					<?php else : ?>
+			
+						<?php get_template_part( 'content', 'none' ); ?>
+			
+					<?php endif; ?>
+			
+					</main><!-- #main -->
+				</section><!-- #primary -->
+			
+			<?php do_action( 'winespace_sidebar' ); ?>
+			<?php get_footer(); ?>
+
+	<?php } ?>	
+			
+<?php } ?>
